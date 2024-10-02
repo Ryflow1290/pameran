47a647cbee477c47e7e65370a7b38cc82fe43481
Reza 1290 <reza.muktasib@gmail.com>
2024-10-02 21:40:39 +0900

    fix breadcrumb

--- resources/views/semua.blade.php
+++ resources/views/semua.blade.php
@@ -115,7 +115,7 @@
                     <div style="color: whitesmoke; font-size: 70%;">Pameran Indonusa</div>
                 </div>
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
